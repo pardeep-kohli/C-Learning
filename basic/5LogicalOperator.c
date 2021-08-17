7d6ac6fe0762e2dcb29f731346fadb9500b20e2c
Pardeep_Kohli <pardeepkohli12@gmail.com>
2021-08-17 09:31:57 +0530

    added more files

--- basic/5LogicalOperator.c
+++ basic/5LogicalOperator.c
@@ -2,10 +2,10 @@
 int main()
 {
     int a, b;
-    a = 34;
+    a = 0;
     b = 6;
     printf("a && b = %d\n", a && b);
     printf("a || b = %d\n", a || b);
-    printf("a !  b = %d\n", a ! b);
+    printf("  !  b = %d\n", !b);
     return 0;
 }
