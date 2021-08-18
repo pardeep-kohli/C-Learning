1c2e4359ea2a946545f2ea4b30336c30bdec9c34
Pardeep_Kohli <pardeepkohli12@gmail.com>
2021-08-18 12:06:37 +0530

    add

---
+++ Exercise1/Multiplicationtable.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+Print Multiplication Table of a Entered Number by User
+*/
+int main()
+{
+    int user;
+    printf("Enter the Number of Table : ");
+    scanf("%d",&user);
+    printf("%d * 1 = %d\n",user,user*1);
+    printf("%d * 2 = %d\n",user,user*2);
+    printf("%d * 3 = %d\n",user,user*3);
+    printf("%d * 4 = %d\n",user,user*4);
+    printf("%d * 5 = %d\n",user,user*5);
+    printf("%d * 6 = %d\n",user,user*6);
+    printf("%d * 7 = %d\n",user,user*7);
+    printf("%d * 8 = %d\n",user,user*8);
+    printf("%d * 9 = %d\n",user,user*9);
+    printf("%d * 10 = %d\n",user,user*10);
+    return 0;
+}
